4015f557e8012f7874ba37a02639eda546d74b31
Chandanb2003 <116443036+Chandanb2003@users.noreply.github.com>
2025-03-18 14:07:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  type = string
+}
+
+variable "instance_size" {
+  type = string
+}
+
+variable "instance_count" {
+  type = number
+}
+
+variable "instance_key" {
+  type = string
+}
+
+variable "aws_region" {
+  type = string
+}
